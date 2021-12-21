725c2af9dc89e92a643685a78c7d72b75624a10b
Dr. Lau Cher Han <laucherhan@gmail.com>
2021-12-22 02:52:59 +0800

    not using new tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,54 @@
 <template>
-  <Tutorial />
+  <div>
+    <div class="max-w-screen-lg mx-auto px-3 pt-20 pb-32">
+      <header class="text-center">
+        <h1 class="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
+          DINE IN NOW AVAILABLE
+        </h1>
+        <p class="text-primary-500 mt-5">Head on over to a joint near you!</p>
+      </header>
+    </div>
+
+    <div class="bg-gray-900 px-10 py-10 text-yellow-400">
+      <p class="text-3xl mb-5">MYBURGERLAB = YOUR BURGER LAB</p>
+      <div class="flex">
+        <div class="w-1/4 bg-gray-700 px-5 py-5 [height:500px]">
+          <div class="text-yellow-400 text-xl font-semibold">
+            FRESHLY BAKED BAMBOO CHARCOAL BUNS
+          </div>
+          <p class="text-white">
+            Fluffy, tasty and oh'so addictive. Our buns are prepared from scratch and
+            baked fresh daily.
+          </p>
+        </div>
+        <div class="w-1/4 bg-gray-700 ml-5 px-5 py-5">
+          <div class="text-yellow-400">PATTIES TO DIE FOR</div>
+          <p class="text-white">
+            From Juicy Beef patties, to Fried Chicken Thighs, to Portobello Mushrooms, or
+            even Tofu patties, you're spoilt for choice.
+          </p>
+        </div>
+        <div class="w-1/4 bg-gray-700 ml-5 px-5 py-5">
+          <div class="text-yellow-400">CRAZY COMBINATIONS</div>
+          <p class="text-white">
+            From Juicy Beef patties, to Fried Chicken Thighs, to Portobello Mushrooms, or
+            even Tofu patties, you're spoilt for choice.
+          </p>
+        </div>
+        <div class="w-1/4 bg-gray-700 ml-5 px-5 py-5">
+          <div class="text-yellow-400">HACK-YOUR-OWN</div>
+          <p class="text-white">
+            From Juicy Beef patties, to Fried Chicken Thighs, to Portobello Mushrooms, or
+            even Tofu patties, you're spoilt for choice.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
